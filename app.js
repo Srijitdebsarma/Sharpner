a7e31e36427654bc06929a13267f5c2d42088012
srijitDebsarma <srijitdebsarma@gmail.com>
2024-05-20 18:04:56 +0530

    Form

--- app.js
+++ app.js
@@ -1,27 +1,42 @@
-const express= require('express');
-const app=express();
-const ejsMate=require('ejs-mate');
+const http = require('http');
+const fs = require('fs');
 const path = require('path');
+const querystring = require('querystring');
 
-//middlewares
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
-app.engine("ejs",ejsMate);  //for using ejsMates 
-
-app.listen(4000, ()=>{   //running our app on 8080
-    console.log('Server is listening on port 8080');
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    fs.readFile(path.join(__dirname, 'form.html'), (err, content) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content);
+    });
+  } else if (req.method === 'POST' && req.url === '/submit') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      const parsedBody = querystring.parse(body);
+      fs.appendFile('data.txt', `${parsedBody.username}\n`, (err) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Internal Server Error');
+          return;
+        }
+        res.writeHead(302, { 'Location': '/' });
+        res.end();
+      });
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
 });
 
-app.get("/",(req,res)=>{
-    res.send("Welcome homWelcome to my Node Js project");
-})
-app.get("/home",(req,res)=>{
-    res.send("Welcome home");
-})
-app.get("/about",(req,res)=>{
-    res.send("Welcome to About Us page");
-})
-app.get("/node",(req,res)=>{
-    res.send(" Welcome to my Node Js project");
-})
-
+server.listen(3000, () => {
+  console.log('Server running at http://127.0.0.1:3000/');
+});
