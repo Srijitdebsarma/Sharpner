3e0468965d01173608dc00ba1acecb7be15a6c1a
srijitDebsarma <srijitdebsarma@gmail.com>
2024-05-20 17:07:39 +0530

    Request/Reaponse

--- app.js
+++ app.js
@@ -8,15 +8,20 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.engine("ejs",ejsMate);  //for using ejsMates 
 
-app.listen(8080, ()=>{   //running our app on 8080
+app.listen(4000, ()=>{   //running our app on 8080
     console.log('Server is listening on port 8080');
 });
 
 app.get("/",(req,res)=>{
-    const multiplication=multiply(5,10);
-    res.render("index.ejs",{multiplication});
+    res.send("Welcome homWelcome to my Node Js project");
+})
+app.get("/home",(req,res)=>{
+    res.send("Welcome home");
+})
+app.get("/about",(req,res)=>{
+    res.send("Welcome to About Us page");
+})
+app.get("/node",(req,res)=>{
+    res.send(" Welcome to my Node Js project");
 })
 
-const multiply=(num1,num2)=>{
-    return (num1*num2);
-}
