89ee8c7b421b659c09eab544874844613e97cf30
srijitDebsarma <srijitdebsarma@gmail.com>
2024-05-30 11:21:22 +0530

    404 page added

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ const shopRoutes=require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:true}))   //to parse the incoming req body
 
-app.use(adminRoutes);
+app.use("/admin",adminRoutes);
 app.use(shopRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404).send("<h1>Page not found</h1>");
+})
 
 app.listen(3000);
 
--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
-router.use("/add_product", (req, res, next) => {
+router.get("/add_product", (req, res, next) => {
     res.send(
-      '<form action="/product" method="POST"><input type="text" placeholder="Add a Product" name="title"><input type="number" placeholder="Add size..." name="size"><button>Add A Product</button></form>'
+      '<form action="/admin/add_product" method="POST"><input type="text" placeholder="Add a Product" name="title"><input type="number" placeholder="Add size..." name="size"><button>Add A Product</button></form>'
     ); //it can send the html
   });
   
-router.post("/product",(req,res,next)=>{
+router.post("/add_product",(req,res,next)=>{
       console.log(req.body);
       res.redirect("/");
   })
