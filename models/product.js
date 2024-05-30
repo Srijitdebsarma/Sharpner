931a5d2a7e045a77040bf138447c0c1048d7c92f
srijitDebsarma <srijitdebsarma@gmail.com>
2024-05-30 16:17:42 +0530

    updated js functionalities

--- models/product.js
+++ models/product.js
@@ -1,13 +1,48 @@
-const product=[];
-module.exports= class Product{
-    constructor(t){
-        this.title=t;
-    }
+const fs = require('fs');
+const path = require('path');
 
-    save(){
-        product.push(this);
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
-    static fetchAll(){
-        return product;
-    }
-}
+  });
+};
+
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    this.id = Math.random().toString();
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      cb(product);
+    });
+  }
+};
