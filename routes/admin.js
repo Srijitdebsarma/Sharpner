5296672c794e2f1a4ffeaf7445d634ab40a3cb6a
srijitDebsarma <srijitdebsarma@gmail.com>
2024-05-30 13:53:34 +0530

    styling added

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const path=require('path');
 const router = express.Router();
 
 router.get("/add_product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'addProduct.html'));
+  res.sendFile(path.join(__dirname, '..', 'views', 'addProduct.html'));
   });
   
 router.post("/add_product",(req,res,next)=>{
--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 
 router.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
 })
 
 
